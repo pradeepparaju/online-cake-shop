ffc3b0172ca86b32eb08e138977b3e16d1de70c5
pradeepparaju <parajulipradeep07@gmail.com>
2024-12-04 20:31:53 +0545

    first commit

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,35 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+
+				<li class="ts-label">Main Menu</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+<li><a href="#"><i class="fa fa-files-o"></i> Categories</a>
+<ul>
+<li><a href="create-brand.php">Create Category</a></li>
+<li><a href="manage-brands.php">Manage Categories</a></li>
+</ul>
+</li>
+<li><a href="#"><i class="fa fa-files-o"></i>Sub Categories</a>
+<ul>
+<li><a href="create-sub-category.php">Create SubCategory</a></li>
+<li><a href="manage-sub-category.php">Manage SubCategories</a></li>
+</ul>
+</li>
+<li><a href="#"><i class="fa fa-sitemap"></i> cake</a>
+					<ul>
+						<li><a href="add-cake.php">Add a Cake</a></li>
+						<li><a href="manage-cake.php">Manage cake</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-Orderings.php"><i class="fa fa-users"></i> Manage Orders</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Contact Us Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Registered Users</a></li>
+		
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+			
+		
+
+
+			</ul>
+		</nav>
